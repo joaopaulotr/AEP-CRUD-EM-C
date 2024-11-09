7926c2b22c5b8192c7b79ff7c632c1e553fe1e3b
João Paulo <joaopaulotraguetta14@gmail.com>
2024-11-09 15:26:52 -0300

    AEP-finalizada

--- AEP2.c
+++ AEP2.c
@@ -3,11 +3,55 @@
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <locale.h>
 
 //==========================================================================================================================================
-    char usuario[100][15]; // VariÃ¡veis globais
-    char senha[100][15];
-    int numusuarios = 0;
+
+char usuario[15][100]; // Variáveis globais
+char senha[15][100];
+int numusuarios = 0;
+
+
+//==========================================================================================================================================
+void lerArquivo() {
+    FILE *arquivo = fopen("usuarios.txt", "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+     while (fscanf(arquivo, "%s %s", usuario[numusuarios], senha[numusuarios]) == 2) {
+         numusuarios++;
+         if (numusuarios >= 15) break;
+     }
+    fclose(arquivo);
+}
+//==========================================================================================================================================
+void salvarCadastro () {
+    FILE *arquivo = fopen("usuarios.txt", "w");
+    if (arquivo == NULL) {
+        printf ("Erro ao carregar o arquivo.\n");
+    }
+    for (int i = 0; i < numusuarios; i++) {
+        fprintf(arquivo, "%s %s\n", usuario [i], senha [i]);
+    }
+    fclose (arquivo);
+}
+//==========================================================================================================================================
+
+void ordenaPosicao() {
+    char *tmp;  
+    for (int i = 0; i < numusuarios - 1; i++) {
+        for (int y = i + 1; y < numusuarios; y++) {  
+            if (strcmp(usuario[i], usuario[y]) > 0) {  
+
+                strcpy(tmp, usuario[i]);
+                strcpy(usuario[i], usuario[y]);
+                strcpy(usuario[y], tmp);
+            }
+        }
+    }
+}
+
 //==========================================================================================================================================
 
 #define CHAVE 5
@@ -20,39 +64,35 @@ void criptografar(char *senha) {
 
 //==========================================================================================================================================
 
-bool verif_senha (const char *senha) {
-    bool s1= false, s2= false, s3= false, s4= false, s5= false;
+bool verif_senha(const char *senha) {
+    bool s1 = false, s2 = false, s3 = false, s4 = false, s5 = false;
     int tam = strlen(senha);
 
+    if (tam >= 8 && tam <= 12){
+        s1 = true; // Validou o tamanho da senha
+        }
+    for (int i = 0; i < tam; i++) {
+        char uni = senha[i]; // Verifica caracter por caracter por meio de ponteiro
 
-        if (tam > 8 && tam < 12) s1 = true; // Validou o tamanho da senha
+        if (uni >= 65 && uni <= 90) {
+            s2 = true; // Verifica letras maiúsculas
+        }
+        if (uni >= 97 && uni <= 122) {
+            s3 = true; // Verifica letras minúsculas
+        }
+        if (uni >= 48 && uni <= 57) {   
+            s4 = true; // Verifica números
+        }
+        if ((uni >= 32 && uni <= 47) || (uni >= 58 && uni <= 64) || (uni >= 91 && uni <= 96) || (uni >= 123 && uni <= 126)) {
+            s5 = true; // Verifica caracteres especiais
+        }
+    }
 
-            for (int i=0; i < tam; i++) {
-                char uni = senha[i]; //verifica caracter por caracter por meio de ponteiro
-
-                if (uni >= 65 && uni <= 90) {
-                    s2 = true; //verif letras maiusculas
-                } 
-                
-                if (uni >= 97 && uni <= 122) {
-                    s3 = true; // verif letras minusculas
-                    }
-                    
-                     if (uni >= 48 && uni <= 57) {
-                        s4 = true;  //verif numeros
-                            }
-                            
-                             if (uni >= 32 && uni <= 47 || uni >= 58 && uni <= 64 || uni >= 91 && uni <= 96 || uni >= 123 && uni <= 126) { 
-                                s5 = true; //verif caract especiais
-                            }
-
-            }
-
-        if (s1 && s2 && s3 && s4 && s5) return true;
-
-    else return false;
+    if (s1 && s2 && s3 && s4 && s5)
+        return true;
+    else
+        return false;
 }
- 
 
 //==========================================================================================================================================
 
@@ -64,161 +104,250 @@ void descriptografar(char *senha) {
 
 //==========================================================================================================================================
 
-//xxx exibirCadastros(){}
-
-//==========================================================================================================================================
-
-//xxx atualizarCadastro(){}
-
-//==========================================================================================================================================
-
-void excluirCadastro () {
-    char usuarioexclusao [100], escolha;
-    int x, y;
-    
-        do  {
-
-            system("cls");  
-            printf ("EXCLUIR: \n");
-            printf ("Qual usuÃ¡rio deseja excluir? \n");
-                scanf ("%s", usuarioexclusao);
-
-            y = buscaUsuario(usuarioexclusao); //Passei a funÃ§Ã£o que busca o Ã­ndice do usuÃ¡rio para ver onde ele esta localizado e se ja existe
-
-            if (y == -1) {  //Se o Ã­ndice for menor que 1, significa que nÃ£o foi encontrado
-
-                printf ("UsuÃ¡rio nÃ£o encontrado! \n");
-                printf ("Digite ENTER para continuar... \n");
-
-                getchar ();
-                getchar ();
-
-            } 
-
-        } while (y == -1);
-
-        getchar ();
-        printf ("Digite 'S' para confirmar a exclusÃ£o: \n");
-            scanf (" %c", &escolha);
-
-        if (toupper(escolha) == 'S') {
-
-            for (int x = y; x < numusuarios; x++) {  //Esse loop Ã© responsÃ¡vel por "arrumar" a lista sem que sobrem espaÃ§os apos tirar um usuario
-                strcpy(usuario[x], usuario[x + 1]);
-                strcpy(senha[x], senha[x + 1]);
-            }
-
-        numusuarios--;
-            printf ("UsuÃ¡rio excluido com sucesso! \n");
-
-        }   else printf ("ExclusÃ£o cancelada! \n"); 
-
+void listarCadastro () {
+printf("============================== \n");
+    for (int i = 0; i < numusuarios; i++) {
+        printf ("%s %s \n",usuario [i], senha [i], "(Senha Criptografada)");        
+    }
+printf("============================== \n");
 }
 
 //==========================================================================================================================================
 
-int buscaUsuario (char *nomeBuscado) {  //FunÃ§Ã£o que busca o Ã­ndice (onde ele esta) do usuÃ¡rio
-        for (int i = 0; i < numusuarios; i++) {
+void atualizarCadastro () {
+    char novousuario [100], escolhaatualiza, novasenha [100];
+    int indice;
 
-            if (strcmp(usuario [i], nomeBuscado) == 0 ) {
-                return i;
-            }
+    do {
+        printf ("Você escolheu atualizar o usuário! \n");
+        printf ("Qual usuário deseja modificar? \n");
+        scanf ("%s", novousuario);
+
+        indice = buscaUsuario(novousuario); 
+
+        if (indice == -1) {
+            printf ("Usuário não encontrado! \n");
+            printf ("Digite ENTER para continuar... \n");
+            getchar ();
+            getchar ();
         }
- 
+    } while (indice == -1);
+
+    printf ("Deseja atualizar usuário [U] ou senha [S] ? \n");
+    getchar();
+    scanf ("%c", &escolhaatualiza);
+
+    if (toupper(escolhaatualiza) == 'U') {
+        printf ("Digite o nome do novo usuário:  \n");
+        scanf ("%s", usuario[indice]);
+        printf ("Usuário atualizado com sucesso! \n");
+
+    } else if (toupper(escolhaatualiza) == 'S') {
+        do {
+            printf ("Digite a nova senha: \n");
+            scanf ("%s", novasenha);
+
+            if (verif_senha(novasenha)) {
+                criptografar(novasenha);
+
+                strcpy (senha[indice], novasenha);
+                printf ("Senha atualizada com sucesso! \n");
+                break;
+            }  else {
+                printf ("A senha digitada não corresponde aos parâmetros desejados, tente novamente... \n");
+                }
+        }  while (!verif_senha(novasenha));
+    } else printf ("A opção digitada não existe! \n"); 
+
+    salvarCadastro();
+}
+
+//==========================================================================================================================================
+
+void excluirCadastro() {
+    char usuarioexclusao[100], escolha;
+    int x, y;
+
+    do {
+        system("cls");
+        printf("EXCLUIR: \n");
+        printf("Qual usuário deseja excluir? \n");
+        scanf("%s", usuarioexclusao);
+
+        y = buscaUsuario(usuarioexclusao); // Busca o índice do usuário para ver onde ele está localizado e se já existe
+
+        if (y == -1) { // Se o índice for -1, significa que não foi encontrado
+            printf("Usuário não encontrado! \n");
+            printf("Digite ENTER para continuar... \n");
+            getchar();
+            getchar();
+        }
+    } while (y == -1);
+
+    getchar();
+    printf("Digite 'S' para confirmar a exclusão: \n");
+    scanf(" %c", &escolha);
+
+    if (toupper(escolha) == 'S') {
+        for (int x = y; x < numusuarios; x++) {
+            // Esse loop é responsável por "arrumar" a lista sem que sobrem espaços após tirar um usuário
+            strcpy(usuario[x], usuario[x + 1]);
+            strcpy(senha[x], senha[x + 1]);
+        }
+        numusuarios--;
+        printf("Usuário excluído com sucesso! \n");
+    } else {
+        printf("Exclusão cancelada! \n");
+    }
+    salvarCadastro();
+}
+
+//==========================================================================================================================================
+
+int buscaUsuario(char *nomeBuscado) {
+    // Função que busca o índice (onde ele está) do usuário
+    for (int i = 0; i < numusuarios; i++) {
+        if (strcmp(usuario[i], nomeBuscado) == 0) {
+            return i;
+        }
+    }
     return -1;
 }
+
 //==========================================================================================================================================
 
-void mostrarMenu() {
+void mostrarMenuAdm() {
     printf("===================================\n");
-    printf("    Bem-vindo ao nosso CRUD em C   \n");
+    printf(" Bem-vindo ao nosso CRUD em C \n");
     printf("===================================\n");
-    printf("1. Criar UsuÃ¡rio\n");
+    printf("1. Criar Usuário\n");
     printf("2. Visualizar Cadastros\n");
     printf("3. Atualizar Cadastros\n");
     printf("4. Excluir Cadastros\n");
     printf("5. Sair\n");
     printf("===================================\n");
-    printf("Escolha uma opÃ§Ã£o:\n ");
+    printf("Escolha uma opção: \n");
+    printf("===================================\n");
+}
+
+//==========================================================================================================================================
+
+void mostrarMenuUsuario() {
+    printf("===================================\n");
+    printf(" Bem-vindo ao nosso CRUD em C \n");
+    printf("===================================\n");
+    printf("1. Criar Usuário\n");
+    printf("2. Atualizar Cadastros\n");
+    printf("3. Sair\n");
+    printf("===================================\n");
+    printf("Escolha uma opção: \n");
     printf("===================================\n");
 }
 
 //==========================================================================================================================================
 
 void opcao1() {
-
-
     printf("===============================\n");
-    printf("VocÃª escolheu Criar um usuÃ¡rio!\n");
+    printf("Você escolheu Criar um usuário!\n");
     printf("===============================\n");
-    printf("Digite o seu usuÃ¡rio:\n");
-    scanf("%s", usuario);
-    
-    do
-    {
-    printf("===============================\n");
-    printf("Agora digite sua senha: \n");
-        scanf("%s", senha);
-    
-    if (verif_senha(senha)){
+    printf("Digite o seu usuário:\n");
+    scanf("%99s", usuario[numusuarios]); // Corrigi a referência ao array de usuários
 
-        criptografar(senha);
-
-    printf("ParabÃ©ns, agora vocÃª estÃ¡ cadastrado.\n");
-    break;
-        }else{
-
-    printf("Senha nÃ£o atingiu os parÃ¢metros desejados, digite novamente.");
-
-    }
-        
-    } while (verif_senha(senha) != false);
+    do {
         printf("===============================\n");
+        printf("Agora digite sua senha: \n");
+        scanf("%s", senha [numusuarios]); // Corrigi a referência ao array de senhas
+
+        if (verif_senha(senha[numusuarios])) {
+            criptografar(senha[numusuarios]);
+            printf("Parabéns, agora você está cadastrado.\n");
+            numusuarios++; // Incrementa o número de usuários
+            salvarCadastro();
+            break;
+        } else {
+            printf("Senha não atingiu os parâmetros desejados, digite novamente.\n");
+        }
+    } while (!verif_senha(senha[numusuarios]));
+
+    printf("===============================\n");
 }
 
 //==========================================================================================================================================
 
 void opcao2() {
     printf("===================================\n");
-    printf("VocÃª escolheu Visualizar cadastros!\n");
+    printf("Você escolheu Visualizar cadastros!\n");
     printf("===================================\n");
+
+    listarCadastro ();
 }
 
 //==========================================================================================================================================
 
 void opcao3() {
     printf("====================================\n");
-    printf("VocÃª escolheu Atualizar um cadastro!\n");
+    printf("Você escolheu Atualizar um cadastro!\n");
     printf("====================================\n");
+
+    atualizarCadastro ();
 }
 
 //==========================================================================================================================================
 
 void opcao4() {
     printf("==================================\n");
-    printf("VocÃª escolheu Excluir um cadastro!\n");
+    printf("Você escolheu Excluir um cadastro!\n");
     printf("==================================\n");
-    excluirCadastro(); 
-    excluirCadastro;
+    excluirCadastro();
 }
+
 //==========================================================================================================================================
 
 void opcao5() {
     printf("==========================================================\n");
-    printf("VocÃª escolheu Sair, muito obrigado por conferir nossa AEP!\n");
+    printf("Você escolheu Sair, muito obrigado por conferir nossa AEP!\n");
     printf("==========================================================\n");
 }
 
 //==========================================================================================================================================
+void infos(){
+        printf("===================================\n");
+        printf("Você escolheu Ver as informações!\n");
+        printf("===================================\n");
+        printf("Bem vindos ao sistema de Gerenciamento\n");
+        printf("de dados, esse CRUD foi desenvolvido em C.\n");
+        printf("====================================\n");
+        printf("Desenvolvido pelos alunos (ESOFT2S-A):\n");
+        printf("João Paulo Traguetta Rufino\n");
+        printf("Heitor Ferrari Crivelin\n");
+        printf("Maria Eduarda Furlan\n");
+        printf("====================================\n");
+ 
+
+}
+//==========================================================================================================================================
 
 
-int main() {
-    
+int main () {
+    int escolhaCargo, escolha;
 
-    int escolha;
+
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("===================================\n");
+    printf(" Bem-vindo ao nosso CRUD em C \n");
+    printf("===================================\n");
+
+
+    printf ("Selecione uma opção:\nUsuário (U)\nAdministrador (A)\nInformações e Créditos (I)\n");
+    printf("===================================\n");
+        scanf ("%c", &escolhaCargo);
+
+    if (toupper(escolhaCargo) == 'A') {
+
+
     do {
-        system("cls");  
-        mostrarMenu();
+        mostrarMenuAdm();
         scanf("%d", &escolha);
 
         switch (escolha) {
@@ -238,14 +367,48 @@ int main() {
                 opcao5();
                 break;
             default:
-                printf("OpÃ§Ã£o invÃ¡lida! Tente novamente.\n");
+                printf("Opção inválida! Tente novamente.\n");
         }
+
+        if (escolha != 5) {
         printf("Pressione Enter para continuar...\n");
         getchar();
-        getchar(); 
-    } while (escolha != 6);
+        getchar();
+        }
 
+    } while (escolha != 5);
+    }  else if(toupper (escolhaCargo) == 'U') {
 
+        do {
+        system("cls");
+        mostrarMenuUsuario();
+        scanf("%d", &escolha);
 
+        switch (escolha) {
+            case 1:
+                opcao1();
+                break;
+            case 2:
+                opcao3();
+                break;
+            case 3:
+                opcao5();
+                break;
+            default:
+                printf("Opção inválida! Tente novamente.\n");
+        }
+
+        if (escolha != 3) {
+        printf("Pressione Enter para continuar...\n");
+        getchar();
+        getchar();
+        }
+
+    } while (escolha != 3);
+    }
+    else if (toupper (escolhaCargo) == 'I'){ 
+          infos();  
+        } else printf ("Opção inválida! Digite novamente... \n");
+        getchar ();
     return 0;
 }
